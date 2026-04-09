e4ac6dced09572f18efdf04351e494fd7c169c41
Shubham2186 <inevitable218@gmail.com>
2026-04-09 12:14:10 +0530

    Used function for Linear Search

--- Linear_Search.c
+++ Linear_Search.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
+int linear_search(int arr[],int key, int n){
+    int i;
+    for(i = 0;i<n;i++) {
+        if(arr[i] == key) {
+        return 1;
+        }
+    }
+    return 0;
+}
 int main() {
     int arr[10] = {1,2,3,4,34,45,56,78,89,100};
     int key;
     printf("Enter element to search : ");
     scanf("%d",&key);
-    int i, ispresent = 0,index;
     int count = sizeof(arr)/sizeof(arr[0]);
-    for (i = 0; i< count; i++) {
-        if(arr[i] == key) {
-            index = i;
-            ispresent = 1;
-            break;
-        }
-    }
-    if(ispresent) printf("Element present in the array at %d position.\n",index+1);
+    if(linear_search(arr,key,count)) printf("Element present \n");
     else printf("Element is not present in the array!\n");
     return 0;
 }
