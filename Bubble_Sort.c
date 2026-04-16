c3fdd58c1e98e3ea198ba3d4121c5c0ec93a9528
Shubham2186 <inevitable218@gmail.com>
2026-04-16 11:28:26 +0530

    Program for Bubble Sort Algorithm

---
+++ Bubble_Sort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void Bubble_Sort(int arr[], int n) {
+    int i,j,temp;
+    for (i = 0;i<n;i++) {
+        for (j = 0; j< n-i-1;j++) {
+            if (arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+int main() {
+    int arr[10] = {2,7,12,24,4,56,71,100,204,1};
+    int count = sizeof(arr)/sizeof(arr[0]), i;
+    printf("Unsorted array : \n");
+    for (i = 0; i < count; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n\nSorted array : \n");
+    Bubble_Sort(arr,count);
+    for (i = 0; i < count; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
