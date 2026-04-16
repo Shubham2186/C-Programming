71f2f80924a1a33609652646cf6a1a59d9922607
Shubham2186 <inevitable218@gmail.com>
2026-04-16 11:42:51 +0530

    Program for Selection Sort

---
+++ Selection_Sort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void Selection_Sort(int arr[], int n) {
+    int i, j, min_index, temp;
+    for (i = 0; i < n - 1; i++) {
+        min_index = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }       
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+}
+int main() {
+    int arr[] = {5, 3, 8, 4, 2};
+    int n = sizeof(arr)/sizeof(arr[0]), i;
+    Selection_Sort(arr, n);
+    printf("Sorted array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
